2cf1d1bba37819c0f8e6c4252abf74a69e478d90
Avallach94 <gacek.kon@gmail.com>
2020-03-19 19:17:37 +0100

    Editing mobile view

--- EmKop/script.js
+++ EmKop/script.js
@@ -10,9 +10,16 @@ let galleryContent = document.querySelector('#gallery .content');
 let mobileMenu = document.querySelector('.mobile-menu i');
 let header = document.querySelector('header');
 let html = document.querySelector('html');
+let navBtn = document.querySelectorAll('header a');
 
 let allPhoto = []
 let isZoom = false;
+let mobMenuOpn = false;
+let isMobile = false;
+
+if (document.querySelector('.mobile-contact').style.visibility = 'visible'){
+    isMobile = true
+}
 
 for (var i = 0; i < gallery.childNodes.length; i++){
     if (gallery.childNodes[i].nodeName == 'IMG'){
@@ -21,6 +28,9 @@ for (var i = 0; i < gallery.childNodes.length; i++){
 }
 
 function resize(){
+    if (isMobile == true) {
+        return
+    }
     let offerHeight = document.querySelector('#offer').offsetHeight;
     let galleryHeight = document.querySelector('#gallery').offsetHeight;
     let contactHeight = document.querySelector('#contact').offsetHeight;
@@ -41,10 +51,14 @@ function nextPhoto() {
                 allPhoto[newIndex].className = 'chose-photo';
             }
         }
-        if (isZoom == true) {
-            document.querySelector('.chose-photo').style.width = '70vw';
+        if (isMobile == false) {
+            if (isZoom == true) {
+                document.querySelector('.chose-photo').style.width = '70vw';
+            } else {
+                document.querySelector('.chose-photo').style.width = '50vw';
+            }
         } else {
-            document.querySelector('.chose-photo').style.width = '50vw';
+            document.querySelector('.chose-photo').style.width = '85vw';
         }
     }
 }
@@ -65,41 +79,56 @@ function prevPhoto() {
 }
 
 function zoom() {
-    if (isZoom == false) {
-        html.style.overflowY = 'hidden';
-        header.style.visibility = 'hidden';
-        photoShadow.style.backgroundColor = 'rgba(61, 61, 61, 0.80)';
-        photoShadow.style.width = '110vw';
-        photoShadow.style.height = '130vh';
-        photoShadow.style.top = '-15vh';
-        photoShadow.style.left = '-20vw';
-        nextPht.style.color = '#C3C3C3';
-        prevPht.style.color = '#C3C3C3';
-        document.querySelector('.chose-photo').style.width = '70vw';
-        isZoom = true;
+    if (isMobile == true) {
+        return 
     } else {
-        header.style.visibility = 'visible';
-        html.style.overflowY = 'scroll';
-        photoShadow.style.backgroundColor = '#3D3D3D';
-        photoShadow.style.width = '50vw';
-        photoShadow.style.height = '77vh';
-        photoShadow.style.top = '13.5vh';
-        photoShadow.style.left = '16.5vw';
-        nextPht.style.color = '#3D3D3D';
-        prevPht.style.color = '#3D3D3D';
-        document.querySelector('.chose-photo').style.width = '50vw';
-        isZoom = false;
+        if (isZoom == false) {
+            html.style.overflowY = 'hidden';
+            header.style.visibility = 'hidden';
+            photoShadow.style.backgroundColor = 'rgba(61, 61, 61, 0.80)';
+            photoShadow.style.width = '110vw';
+            photoShadow.style.height = '130vh';
+            photoShadow.style.top = '-15vh';
+            photoShadow.style.left = '-20vw';
+            nextPht.style.color = '#C3C3C3';
+            prevPht.style.color = '#C3C3C3';
+            document.querySelector('.chose-photo').style.width = '70vw';
+            isZoom = true;
+        } else {
+            header.style.visibility = 'visible';
+            html.style.overflowY = 'scroll';
+            photoShadow.style.backgroundColor = '#3D3D3D';
+            photoShadow.style.width = '50vw';
+            photoShadow.style.height = '77vh';
+            photoShadow.style.top = '13.5vh';
+            photoShadow.style.left = '16.5vw';
+            nextPht.style.color = '#3D3D3D';
+            prevPht.style.color = '#3D3D3D';
+            document.querySelector('.chose-photo').style.width = '50vw';
+            isZoom = false;
+        }
     }
-    
 }
 
 function openMenu() {
-    header.style.left = '0';
+    header.style.visibility = 'visible';
+    header.style.zIndex = '3';
     html.style.overflowY = 'hidden';
+    mobMenuOpn = true;
+}
+
+function closeMenu() {
+    header.style.visibility = 'hidden';
+    header.style.zIndex = '-3';
+    html.style.overflowY = 'scroll';
+    mobMenuOpn = false;
 }
 
 window.addEventListener('resize', resize);
 nextPht.addEventListener('click', nextPhoto);
 prevPht.addEventListener('click', prevPhoto);
 photo.addEventListener('click', zoom);
-mobileMenu.addEventListener('focus', openMenu);
+mobileMenu.addEventListener('click', openMenu);
+for (i = 0; i < navBtn.length; i++) {
+    navBtn[i].addEventListener('click', closeMenu);
+}
