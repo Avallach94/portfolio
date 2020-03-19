8d984405f7ea9d669b7d428b0278456306b2db6e
Avallach94 <gacek.kon@gmail.com>
2020-03-19 18:14:23 +0100

    working with mobile view

--- EmKop/script.js
+++ EmKop/script.js
@@ -7,6 +7,9 @@ let prevPht = document.querySelector('#prev-photo');
 let photo = document.querySelector('.gallery-photo');
 let photoShadow = document.querySelector('.gallery-img-shadow');
 let galleryContent = document.querySelector('#gallery .content');
+let mobileMenu = document.querySelector('.mobile-menu i');
+let header = document.querySelector('header');
+let html = document.querySelector('html');
 
 let allPhoto = []
 let isZoom = false;
@@ -63,8 +66,8 @@ function prevPhoto() {
 
 function zoom() {
     if (isZoom == false) {
-        document.querySelector('html').style.overflowY = 'hidden';
-        document.querySelector('header').style.visibility = 'hidden';
+        html.style.overflowY = 'hidden';
+        header.style.visibility = 'hidden';
         photoShadow.style.backgroundColor = 'rgba(61, 61, 61, 0.80)';
         photoShadow.style.width = '110vw';
         photoShadow.style.height = '130vh';
@@ -75,8 +78,8 @@ function zoom() {
         document.querySelector('.chose-photo').style.width = '70vw';
         isZoom = true;
     } else {
-        document.querySelector('header').style.visibility = 'visible';
-        document.querySelector('html').style.overflowY = 'scroll';
+        header.style.visibility = 'visible';
+        html.style.overflowY = 'scroll';
         photoShadow.style.backgroundColor = '#3D3D3D';
         photoShadow.style.width = '50vw';
         photoShadow.style.height = '77vh';
@@ -90,7 +93,13 @@ function zoom() {
     
 }
 
+function openMenu() {
+    header.style.left = '0';
+    html.style.overflowY = 'hidden';
+}
+
 window.addEventListener('resize', resize);
 nextPht.addEventListener('click', nextPhoto);
 prevPht.addEventListener('click', prevPhoto);
 photo.addEventListener('click', zoom);
+mobileMenu.addEventListener('focus', openMenu);
