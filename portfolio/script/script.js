195d747bf5999fa9420d46066348ea6a5d8397fd
Avallach94 <gacek.kon@gmail.com>
2020-09-26 12:48:09 +0200

    new animation

---
+++ portfolio/script/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $("h1").animate({
+        opacity: 1,
+        left: 0,
+    }, 3000)
+});
+
+$(document).ready(function() {
+    $(".name").animate({
+        color: '#44BD32',
+    }, 2000)
+    $("#about p").animate({
+        opacity: 0,
+    }, 2000)
+    $("#about p").animate({
+        opacity: 1,
+        left: 0,
+    }, 3000)
+});
+
